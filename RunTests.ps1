7d942a96b8b002dfedf9960b24d420d32b71bec5
paulocmi <ppaulocm@gmail.com>
2017-09-27 14:34:06 -0300

    Add script to run tests

---
+++ RunTests.ps1
@@ -0,0 +1,23 @@
+trap
+{
+    Write-Error $_
+    exit 1
+}
+function Exec
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [scriptblock]$Command,
+        [Parameter(Position=1, Mandatory=0)]
+        [string]$ErrorMessage = "Execution of command failed.`n$Command"
+    )
+    & $Command
+    if ($LastExitCode -ne 0) {
+        throw "Exec: $ErrorMessage"
+    }
+}
+
+Exec { npm install -g newman }
+Exec { npm install -g newman-reporter-teamcity }
+Exec { newman run tests/00_TeamCity.postman_collection -g environments/globals.postman_globals.json -r cli`,teamcity --disable-unicode --no-color }
